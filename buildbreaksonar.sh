134714622bffbd1ac728b41b176ef13f15d8540d
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2018-06-14 20:51:08 -0300

    Adicionando mensagem de url

--- buildbreaksonar.sh
+++ buildbreaksonar.sh
@@ -4,13 +4,15 @@
     echo "Starting Scrip"
 
     echo "Searching SonnarQueb AnalysisId in $1" 
-
+    
     url=$(grep "/api/ce/task?id=" $1 | sed s/.*h// )
 
     echo "Searching at h$url"
 
     analysisId=$(curl -u $2: "h$url" | jq -r '.task.analysisId')
-
+    
+    echo "Searching at $3/api/qualitygates/project_status?analysisId=$analysisId"
+    
     status=$(curl -u $2: "$3/api/qualitygates/project_status?analysisId=$analysisId" | jq -r '.projectStatus.status')
 
     if [ "$status" == "ERROR" ]; then 
