90b9c8cb5d56bfe4a6e9828d80f726f94307e723
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2018-06-14 13:31:54 -0300

    Adicionando o JQ para varrer o json

--- buildbreaksonar.sh
+++ buildbreaksonar.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
+#sh buildbreaksonar.sh sonar.txt c8fa3ca2a1b63fd4209ad71670e16d612daa416e (token)
 echo Starting Scrip
-echo Searching SonnarQueb AnalysisId in $1
-grep "/api/ce/task?id=" $1 | sed s/.*id=//
 
+echo Searching SonnarQueb AnalysisId in $1 
 
+url=$(grep "/api/ce/task?id=" $1 | sed s/.*h// )
 
+echo "Searching at h$url"
+
+analysisId=$(curl -u $2: "h$url" | jq -r '.task.analysisId')
+
+status=$(curl -u $2: "https://sonarcloud.io/api/qualitygates/project_status?analysisId=$analysisId" | jq -r '.projectStatus.status')
+
+echo "$status"
