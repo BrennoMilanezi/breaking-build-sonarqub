2cb40eafa44b0e677704e0baa7e7c85626104a96
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2018-06-14 21:00:03 -0300

    modificando o travis

--- buildbreaksonar.sh
+++ buildbreaksonar.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 #sh buildbreaksonar.sh sonar.txt c8fa3ca2a1b63fd4209ad71670e16d612daa416e (token) https://sonarcloud.io
 
-    echo "Starting Scrip"
+echo "Starting Scrip"
 
-    echo "Searching SonnarQueb AnalysisId in $1" 
+echo "Searching SonnarQueb AnalysisId in $1" 
     
-    url=$(grep "/api/ce/task?id=" $1 | sed s/.*h// )
+url=$(grep "/api/ce/task?id=" $1 | sed s/.*h// )
 
-    echo "Searching at h$url"
+echo "Searching at h$url"
 
-    analysisId=$(curl -u $2: "h$url" | jq -r '.task.analysisId')
+analysisId=$(curl -u $2: "h$url" | jq -r '.task.analysisId')
+
+echo "AnalysisID: $analysisId"
+
+echo "Searching at $3/api/qualitygates/project_status?analysisId=$analysisId"
     
-    echo "Searching at $3/api/qualitygates/project_status?analysisId=$analysisId"
-    
-    status=$(curl -u $2: "$3/api/qualitygates/project_status?analysisId=$analysisId" | jq -r '.projectStatus.status')
+status=$(curl -u $2: "$3/api/qualitygates/project_status?analysisId=$analysisId" | jq -r '.projectStatus.status')
 
-    echo "Status: $status" 
+echo "Status: $status" 
 
-    if [ $status == "ERROR" ]; then 
-        echo "ERROR"
-    fi
+if [ $status == "ERROR" ]; then 
+    echo "ERROR"
+fi
